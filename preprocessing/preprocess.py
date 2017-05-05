eef0996421b50fb99ce98f1dfc4f8ccdc9549eed
idrojsnop <idrojsnop@gmail.com>
2017-05-05 08:23:57 +0200

    improve readability of preprocess

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,21 +1,13 @@
 import json
 
-def fetch_mark (restrictions):
-	marks=[]
-	if "abstract" in restrictions:
-		marks.append("abstract")
-	if "blacklist" in restrictions:
-		marks.append("blacklist")
-	return marks
-
 def get_all_children(category,aso):
 	childs = aso[category]["child_ids"]
-	childs_names=[]
+	childs_names = []
 	for child in childs:
 		child_name = {}
 		child_name["name"] = aso[child]["name"]
-		child_name["mark"]=fetch_mark(aso[child]["restrictions"])
-		if "child_ids" in aso[child]: child_name["children"]=get_all_children(child,aso)
+		child_name["mark"] = aso[child]["restrictions"]
+		if "child_ids" in aso[child]: child_name["children"] = get_all_children(child,aso)
 		childs_names.append(child_name)
 	if childs_names : return childs_names
 
@@ -27,14 +19,14 @@ with open('ontology.json') as data_file:
 
 # 1. format data as a dictionary
 ## aso["/m/0dgw9r"] > {'restrictions': [u'abstract'], 'child_ids': [u'/m/09l8g', u'/m/01w250', u'/m/09hlz4', u'/m/0bpl036', u'/m/0160x5', u'/m/0k65p', u'/m/01jg02', u'/m/04xp5v', u'/t/dd00012'], 'name': u'Human sounds'}
-aso={}
+aso = {}
 for category in raw_aso:
-	tmp={}
-	tmp["name"]=category["name"]	
-	tmp["restrictions"]=category["restrictions"]
-	tmp["child_ids"]=category["child_ids"]
-	tmp["parents_ids"]=[]
-	aso[category["id"]]=tmp
+	tmp = {}
+	tmp["name"] = category["name"]	
+	tmp["restrictions"] = category["restrictions"]
+	tmp["child_ids"] = category["child_ids"]
+	tmp["parents_ids"] = []
+	aso[category["id"]] = tmp
 
 # 2. fetch higher_categories > ["/m/0dgw9r","/m/0jbk","/m/04rlf","/t/dd00098","/t/dd00041","/m/059j3w","/t/dd00123"]
 for cat in aso: # find parents
@@ -43,18 +35,18 @@ for cat in aso: # find parents
 
 higher_categories=[] # higher_categories are the ones without parents
 for cat in aso: 
-	if aso[cat]["parents_ids"]==[]:
+	if aso[cat]["parents_ids"] == []:
 		higher_categories.append(cat)
 
 # 3. format ASO properly
-out_json={}
-out_json["name"]="Ontology"
-out_json["children"]=[]
+out_json = {}
+out_json["name"] = "Ontology"
+out_json["children"] = []
 for category in higher_categories:
 	dict_level1 = {}
 	dict_level1["name"] = aso[category]["name"]
-	dict_level1["mark"]=fetch_mark(aso[category]["restrictions"])
-	dict_level1["children"]=get_all_children(category,aso)
+	dict_level1["mark"] = aso[category]["restrictions"]
+	dict_level1["children"] = get_all_children(category,aso)
 	out_json["children"].append(dict_level1)
 
 # 4. saving output .json
